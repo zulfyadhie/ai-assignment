9ae9ca6ebe533ba638ccc805a747a95a1cfd6028
Zulfy Adhie <zulfy.adhie@gmail.com>
2016-09-12 14:16:01 +0700

    Initial framework

---
+++ src/main/java/com/dpbg/AiAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.dpbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssignmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dpbg/AiAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dpbg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AiAssignmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
